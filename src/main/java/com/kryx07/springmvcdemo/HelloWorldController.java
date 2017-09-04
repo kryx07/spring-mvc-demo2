41bf1b96a3b7a12808b900d005e4fc6ad0483e6f
kryx07 <kryx07@gmail.com>
2017-09-04 12:47:02 +0200

    Initial Commit

---
+++ src/main/java/com/kryx07/springmvcdemo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.kryx07.springmvcdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("showForm")
+    public String showForm(){
+        return "show-form";
+    }
+}
---
+++ web/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<body>
+
+<h2>Spring MVC Demo - Home Page</h2>
+
+Dupa
+
+<%--<br> <a href="/showForm">Hello World form</a>
+
+<br><br>
+<br> <a href="/student/showForm">Student form</a>--%>
+</body>
+
+</html>
